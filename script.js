16477e1d228e77c9e2e984de84696e3e6eac60b3
Farhad <farhadrajib@hotmail.com>
2016-02-08 21:32:11 +0100

    Fianl app

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-var mybutton = document.getElementById('loadbutton');
-mybutton.onclick = function() {
-    var request;
-    if (window.XMLHttpRequest) {
-        request = new XMLHttpRequest();
-    } else {
-        request = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    request.open('GET', 'data.json');
-    request.onreadystatechange = function() {
-        if ((request.readyState===4) && (request.status===200)) {
-            var items = JSON.parse(request.responseText);
-            var output = '<ul>';
-            for (var key in items) {
-                output += '<li>' + items[key].name + '</li>';
+$('#search').keyup(function() {
+    var searchField = $('#search').val();
+    var myExp = new RegExp(searchField, "i");
+    $.getJSON('data.json', function(data) {
+        var output = '<ul class="searchresults">';
+        $.each(data, function(key, val) {
+            if ((val.name.search(myExp) != -1) ||
+            (val.bio.search(myExp) != -1)) {
+                output += '<li>';
+                output += '<h2>'+ val.name +'</h2>';
+                output += '<img src="images/'+ val.shortname +'_tn.jpg" alt="'+ val.name +'" />';
+                output += '<p>'+ val.bio +'</p>';
+                output += '</li>';
             }
-            output += '</ul>';
-            document.getElementById('update').innerHTML = output;
-        }
-    }
-    request.send();
-} // loadAJAX
+        });
+        output += '</ul>';
+        $('#update').html(output);
+    }); //get JSON
+});
